03cd4f383e48ea09069d04d55333a71041d5fee0
drdarian <dariantaban@gmail.com>
2021-07-26 17:07:39 -0700

    avatar arrow key movements

--- app.js
+++ app.js
@@ -9,3 +9,29 @@ function isTouching(a, b) {
 		aRect.left > bRect.left + bRect.width
 	);
 }
+
+const avatar = document.querySelector('#player');
+
+window.addEventListener('keyup', function(e) {
+	if(e.key === 'ArrowDown') {
+		const currTop = extractPos(avatar.style.top);
+		avatar.style.top = `${currTop + 50}px`;
+	}
+	else if (e.key === 'ArrowUp') {
+		const currTop = extractPos(avatar.style.top);
+		avatar.style.top = `${currTop - 50}px`
+	}
+	else if (e.key === 'ArrowRight') {
+	const currLeft = extractPos(avatar.style.left);
+	avatar.style.left = `${currLeft + 50}px`
+	}
+	else if (e.key === 'ArrowLeft') {
+		const currLeft = extractPos(avatar.style.left);
+		avatar.style.left = `${currLeft - 50}px`
+	}
+});
+
+const extractPos = (pos) =>  {
+	if (!pos) return 100;
+	return parseInt(pos.slice(0, -2))
+}
